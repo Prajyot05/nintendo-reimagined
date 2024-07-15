3344105271b2de67e39915429094095dccdd0387
Prajyot Tayde <pmtbmt@gmail.com>
2024-07-15 20:41:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import NavBar from './components/NavBar'
+import HomePage from './pages/HomePage'
+
+function App() {
+
+  return (
+    <div className='app bg-black min-h-screen'>
+      <NavBar />
+      <div className="main">
+        <HomePage />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='home w-full flex justify-center p-20'>
+        <h1 className='text-white text-[13rem] text3d'>NINTENDO</h1>
+    </div>
+  )
+}
+
+export default HomePage
