5d2caa493cafb9ccb5ad871d6f07a86c24131937
Sahil <132002159+sevil-git@users.noreply.github.com>
2024-07-16 23:05:08 +0530

    Update1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import './App.css'
-import NavBar from './components/NavBar'
-import HomePage from './pages/HomePage'
+import { ScrollControls, Scroll} from "@react-three/drei"
+
 
 function App() {
 
   return (
-    <div className='app bg-black min-h-screen'>
-      <NavBar />
-      <div className="main">
-        <HomePage />
-      </div>
-    </div>
+    <>
+      <color attach="background" args={['#0d0d0d']}/>
+      <ambientLight intensity={2} />
+      <spotLight position={0, 25, 0} angle={1.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001}/>
+      <ScrollControls pages={6} damping={0.1}>
+        {/* No Scroll fixed components*/}
+        <Scroll>
+         {/* Scrollable 3d components */}
+        </Scroll>
+        <Scroll html>
+          Hola Amigo
+        </Scroll>
+      </ScrollControls>
+      
+    </>
   )
 }
 
