16aa7789c2eeaa1630dc38ddb8e52f60c22f3cd7
Sahil <132002159+sevil-git@users.noreply.github.com>
2024-07-17 19:07:26 +0530

    added3d model

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { ScrollControls, Scroll} from "@react-three/drei"
+import NavBar from "./components/NavBar"
+import HomePage from "./components/HomePage"
+import CanvasContainer from "./components/CanvasContainer"
 
-
-function App() {
+const App = () => {
 
   return (
     <>
-      <color attach="background" args={['#0d0d0d']}/>
-      <ambientLight intensity={2} />
-      <spotLight position={0, 25, 0} angle={1.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001}/>
-      <ScrollControls pages={6} damping={0.1}>
-        {/* No Scroll fixed components*/}
-        <Scroll>
-         {/* Scrollable 3d components */}
-        </Scroll>
-        <Scroll html>
-          Hola Amigo
-        </Scroll>
-      </ScrollControls>
+      <div className=" fixed w-screen h-full z-[99]">
+        <CanvasContainer/>
+      </div>
       
+      <NavBar/>
+      <HomePage/>
     </>
   )
 }
