14cc6cf4f7384d1922296dfec4bc8f36e535ce05
Sahil <132002159+sevil-git@users.noreply.github.com>
2024-07-25 22:32:47 +0530

    merge1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import { ScrollControls, Scroll} from "@react-three/drei"
+import React from 'react'
 
-
-function App() {
-
+const App = () => {
   return (
-    <>
-      <color attach="background" args={['#0d0d0d']}/>
-      <ambientLight intensity={2} />
-      <spotLight position={0, 25, 0} angle={1.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001}/>
-      <ScrollControls pages={6} damping={0.1}>
-        {/* No Scroll fixed components*/}
-        <Scroll>
-         {/* Scrollable 3d components */}
-        </Scroll>
-        <Scroll html>
-          Hola Amigo
-        </Scroll>
-      </ScrollControls>
+    <div>
       
-    </>
+    </div>
   )
 }
 
 export default App
+
